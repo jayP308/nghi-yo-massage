b386838f9278ffc875302cc99e512e821b7cb2cc
JP Padilla <jp@JPs-MacBook-Air.local>
2023-09-03 17:54:58 -0700

    added login and signup button to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,59 @@
 import React from "react";
-import { Grid, } from "@material-ui/core";
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton from '@mui/joy/ListItemButton';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import HouseSidingIcon from '@mui/icons-material/HouseSiding';
+import { Button, Typography, } from "@material-ui/core";
+
 
 const Header = () => {
     return (
         <>
             <div
-                style={{
-                    width: "100%",
-                    height: "100px",
-                    marginBottom: "25px",
-                    background: "navy",
-                }}
+            style={{
+                width: "100%",
+                height: "150px",
+                marginBottom: "25px",
+                background: "#228B22",
+                textAlign: 'center',
+            }}
             >
-                <Grid xs={2}>
-                    <ListItem role="none">
-                        <ListItemButton
-                        sx={{
-                            color: 'white',
-                        }}
-                        >
-                            <ListItemDecorator>
-                                <HouseSidingIcon/>
-                            </ListItemDecorator>
-                            Home
-                        </ListItemButton>
-                    </ListItem>
-                </Grid>
+
+            <Typography
+            style={{
+                fontSize: "45px",
+                fontFamily: 'monospace',
+                marginTop: '10px',
+                marginBottom: '10px',
+            }}
+            >
+            NGHI YO MASSAGE
+            </Typography>
+
+            <div>
+                <Button
+                href="/login"
+                style={{
+                    fontSize: "20px",
+                    fontFamily: 'monospace',
+                    border: 'solid',
+                    padding: '7px',
+                    marginRight: '20px',
+                    color: 'black'
+                }}
+                >
+                Login
+                </Button>
+                <Button
+                 href="/signup"
+                 style={{
+                    fontSize: "20px",
+                    fontFamily: 'monospace',
+                    border: 'solid',
+                    padding: '7px',
+                    marginRight: '20px'
+                }}
+                >
+                Signup
+                </Button>
+            </div>
+            
             </div>
         </>
     );

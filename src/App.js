6ab383dbb3f18f8c9d0aaa020ac06bf4735e8d81
Junnel Padilla <junnelpadilla307@gmail.com>
2023-09-18 09:31:31 -0700

    changed route from .com to .io to package.json

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <>
           <Routes>
 
-            <Route path="/nghi-yo-massage" element={<Home/>} />
+            <Route path="/" element={<Home/>} />
 
             <Route path="/appointment" element={<Appointment/>} />
 

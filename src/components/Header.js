01f3811eb6f0815b1fb949bfd02a7535a5173276
Junnel Padilla <junnelpadilla307@gmail.com>
2023-09-18 17:15:08 -0700

    changed routes to app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
             >
             <div>
                 <Button
-                href="/"
+                href="/nghi-yo-massage"
                 value='0'
                 style={{
                     fontFamily: 'monospace',

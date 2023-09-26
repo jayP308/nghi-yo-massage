dc6285763ed525530bf6a2fb531ffe4fc1db7782
JP Padilla <junnelpadilla307@gmail.com>
2023-09-26 10:54:58 -0700

    added server.js file for backend work

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`App is listening to http://localhost:${PORT}`));
+
